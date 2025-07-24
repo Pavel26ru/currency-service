ca21c2aceca34e9a98e962b25b3f9914ef480fa6
Pavel26ru <pavel333rus26@gmail.com>
2025-07-24 12:41:52 +0300

    Auth-Service. JWT

---
+++ auth/cmd/auth/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/vctrl/currency-service/auth/internal/handler" // поправь путь под свой модуль
+	"github.com/vctrl/currency-service/pkg/currency"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	currency.RegisterAuthServiceServer(grpcServer, handler.NewAuthServer("your_jwt_secret"))
+	log.Println("Auth gRPC server listening on :8080")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
